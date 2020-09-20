93217a63408e9f5632b09361566c8935e6e939e5
Karina Guerra <kpguerra96@gmail.com>
2020-09-20 18:21:15 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -3,7 +3,7 @@ def find_max_value(array)
   count = 0
   highest_number = 0
   
-  while count < array.length
+  while count < array.length do
     if array[count] > highest_number
       highest_number = array[count]
       count += 1
